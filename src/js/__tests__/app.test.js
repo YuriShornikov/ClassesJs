e76df469803abae024d60c32c9dc4acc22b49354
Yuri Shornikov <dbonlyfun@gmail.com>
2024-02-07 15:43:50 +0300

    add all

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,21 @@
+// import Character from "../app";
+
+// describe('Character class', () => {
+//     test('correct of characters', () => {
+//         const Legolas = new Character('Legolas', 'Bowman');
+//         expect(Legolas).toEqual({
+//             name: 'Legolas',
+//             type: 'Bowman',
+//             health: 100,
+//             level: 1,
+//             attack: 25,
+//             defence: 25,
+//         });
+//     });
+//     test('correct error of name', () => {
+//         expect(() => new Character('A', 'Bowman')).toThrow('Invaled name');
+//     });
+//     test('correct error of type', () => {
+//         expect(() => new Character('Legolas', 'Assasin')).toThrow('Invalid character type');
+//     })
+// });
